8a359885e2f997e2c9fa8498653cee33a1cbe40e
Robert Snyder <r.snyder49@gmail.com>
2018-12-18 00:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 function scuberGreetingForFeet(num) {
-  if (num === undefined) {
+  if (num <= undefined) {
     console.log("This one is on me!");
   }
   else if (num > 2000) {
