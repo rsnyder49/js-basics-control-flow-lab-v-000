b7564789194015908a871703e19eded7548c5cfe
Robert Snyder <r.snyder49@gmail.com>
2018-12-18 00:05:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function scuberGreetingForFeet(num) {
   if (num <= 400) {
     return "This one is on me!";
   }
-  if (num > 2000) {
+  else if (num > 2000) {
     return "I will gladly take your thirty bucks";
   }
-  if (num > 2500) {
+  else if (num > 2500) {
     return "No can do";
   }
 }
